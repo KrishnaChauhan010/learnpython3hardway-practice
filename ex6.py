2971290f37f626ba3401ec4c28c55a83c75c6392
KrishnaChauhan010 <90909593+KrishnaChauhan010@users.noreply.github.com>
2022-07-24 19:32:51 +0530

    Add files via upload

---
+++ ex6.py
@@ -0,0 +1,20 @@
+types_of_peoples = 10
+x = f"There are {types_of_peoples} types of people."
+
+binary = "binary"
+do_not = "don't"
+y = f"Those who know {binary} and those who {do_not}."
+
+print(x)
+print(y)
+
+print(f"I said: {x}")
+print(f"I also said: {y}")
+
+hilarious = False
+joke_eval = "Isn't that joke so fummy?! {}"
+print(joke_eval.format(hilarious))
+
+w = "this is the left side of ..."
+e = "this is the right side of ..."
+print(w + e)
