2343baf1fcae1ded789453e5b8e565940c097dd2
KrishnaChauhan010 <90909593+KrishnaChauhan010@users.noreply.github.com>
2022-07-25 09:56:11 +0530

    Add files via upload

---
+++ Exercises/ex2.py
@@ -0,0 +1,9 @@
+#Exercise
+#A comment, this is so you can read your program later.
+#anything after the # is ignored in python.
+
+print("I could have code like this.") # and the comment after this # is ignored
+
+#You can also use a comment to "disable" or comment out code.
+#print("This won't run.")
+print("But it will!")
