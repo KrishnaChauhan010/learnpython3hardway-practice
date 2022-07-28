14874723247a9639e9c1410ff1d7f02f4f0eab99
KrishnaChauhan010 <90909593+KrishnaChauhan010@users.noreply.github.com>
2022-07-28 17:22:30 +0530

    Add files via upload

---
+++ Exercises/ex13.py
@@ -0,0 +1,27 @@
+from sys import argv
+#read the WYSS section for how to run this
+script, first, second, third = argv
+
+print("This script is called:", script)
+print("your first variable is:", first)
+print("your second variable is:", second)
+print("your third variable is:", third)
+
+#run command
+"""
+cd to path/
+python ex13.py first 2nd third
+python ex13.py apple orange banana
+"""
+
+"""
+st1 = int(input())
+nd2 = int(input())
+rd3 = int(input())
+th4 = int(input())
+
+print("This script is called:", st1)
+print("your first variable is:", nd2)
+print("your second variable is:", rd3)
+print("your third variable is:", th4)
+"""
